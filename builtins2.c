1f728ad24219d4878b3c0dc36b77cf7ebedaddf7
Felix <111078933+fbicuul@users.noreply.github.com>
2022-11-14 11:49:54 +0100

    Task on the simple shell

--- builtins2.c
+++ builtins2.c
@@ -1,1 +1,21 @@
-ixmmnmnvxvx
+#include "main.h"
+
+/**
+ * new_cd - To change the current working directory .
+ * if no parameter is passed it will change directory to HOME.
+ * @vars: Indicate a string representing the input from the user.
+ */
+void new_cd(vars_t *vars)
+{
+	int index;
+
+	if (vars->array_tokens[1] == NULL)
+	{
+		index = find_env_index(*vars, "HOME");
+		chdir((vars->env[index]) + 5);
+	}
+	else if (_strcmpr(vars->array_tokens[1], "-") == 0)
+		print_str(vars->array_tokens[1], 0);
+	else
+		chdir(vars->array_tokens[1]);
+}
