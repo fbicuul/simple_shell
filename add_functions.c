c91fac1ec2c42ef27495bc7036a3a39cc58b0523
Felix <111078933+fbicuul@users.noreply.github.com>
2022-11-14 11:52:33 +0100

    Task on the simple shell

--- add_functions.c
+++ add_functions.c
@@ -1,121 +1,61 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * _strcmp - To compare two strings
- *
- *   * @s1: First string
- *
- *    * @s2: Second string
- *
- *     * Return: 0 if strings match. -1 Otherwise.
- *
- *      */
-
+ * _strcmp - To compare two strings
+ * @s1: First string
+ * @s2: Second string
+ * Return: 0 if strings match. -1 Otherwise.
+ */
 int _strcmp(char *s1, char *s2)
-
 {
+	int i;
 
-		int i;
-
-
-
-			if (_strlen(s1) != _strlen(s2))
-
-						return (-1);
-
-				for (i = 0; s1[i] != '\0'; i++)
-
-						{
-
-									if (s1[i] != s2[i])
-
-													return (-1);
-
-										}
-
-					return (0);
-
+	if (_strlen(s1) != _strlen(s2))
+		return (-1);
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (-1);
+	}
+	return (0);
 }
 
-
-
 /**
- *
- *  * error_printing - To print a message error when a comand is not found.
- *
- *   * @count: A counter keeping track of the number of commands run on the shell.
- *
- *    * @av: Indicate the name of the program running the shell.
- *
- *     * @command: Indicate the specific command not being found.
- *
- *      */
-
+ * error_printing - To print a message error when a comand is not found.
+ * @count: A counter keeping track of the number of commands run on the shell.
+ * @av: Indicate the name of the program running the shell.
+ * @command: Indicate the specific command not being found.
+ */
 void error_printing(char *av, int count, char *command)
-
 {
-
-		print_str(av, 1);
-
-			print_str(": ", 1);
-
-				print_number(count);
-
-					print_str(": ", 1);
-
-						print_str(command, 1);
-
+	print_str(av, 1);
+	print_str(": ", 1);
+	print_number(count);
+	print_str(": ", 1);
+	print_str(command, 1);
 }
 
-
-
 /**
- *
- *  * exec_error - To print execute errors.
- *
- *   * @av: Indicate the name of the program running the shell.
- *
- *    * @count: To keep track of how many commands have been entered.
- *
- *     * @tmp_command: Indicate the command that filed.
- *
- *      */
-
-
+ * exec_error - To print execute errors.
+ * @av: Indicate the name of the program running the shell.
+ * @count: To keep track of how many commands have been entered.
+ * @tmp_command: Indicate the command that filed.
+ */
 
 void exec_error(char *av, int count, char *tmp_command)
-
 {
-
-		error_printing(av, count, tmp_command);
-
-			print_str(": ", 1);
-
-				perror("");
-
-					exit(1);
-
+	error_printing(av, count, tmp_command);
+	print_str(": ", 1);
+	perror("");
+	exit(1);
 }
 
-
-
 /**
- *
- *  * simple_error - To print and execute simple error.
- *
- *   * Return: always void.
- *
- *    */
-
+ * simple_error - To print and execute simple error.
+ * Return: always void.
+ */
 void simple_error(void)
-
 {
-
-		perror("Fatal Error");
-
-			exit(100);
-
+	perror("Fatal Error");
+	exit(100);
 }
