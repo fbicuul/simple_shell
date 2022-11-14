e3bd6c15687e000b7fe88d98ac8f8e2d48612e0d
Felix <111078933+fbicuul@users.noreply.github.com>
2022-11-14 10:22:32 +0100

    Task on the simple shell

--- setenv_functions.c
+++ setenv_functions.c
@@ -1,183 +1,92 @@
 #include "main.h"
 
-
-
 /**
+ * add_value - To create a new environment variable string
+ * @key: Indicate variable name
+ * @value: Indicate variable value
  *
- *  * add_value - To create a new environment variable string
- *
- *   * @key: Indicate variable name
- *
- *    * @value: Indicate variable value
- *
- *     *
- *
- *      * Return: pointer to the new string;
- *
- *       */
-
-
+ * Return: pointer to the new string;
+ */
 
 char *add_value(char *key, char *value)
-
 {
+	unsigned int len1, len2, i, j;
+	char *new;
 
-		unsigned int len1, len2, i, j;
-
-			char *new;
-
-
-
-				len1 = _strlen(key);
-
-					len2 = _strlen(value);
-
-						new = malloc(sizeof(char) * (len1 + len2 + 2));
-
-							if (new == NULL)
-
-										return (NULL);
-
-								for (i = 0; key[i] != '\0'; i++)
-
-											new[i] = key[i];
-
-									new[i] = '=';
-
-										for (j = 0; value[j] != '\0'; j++)
-
-													new[i + 1 + j] = value[j];
-
-											new[i + 1 + j] = '\0';
-
-												return (new);
-
+	len1 = _strlen(key);
+	len2 = _strlen(value);
+	new = malloc(sizeof(char) * (len1 + len2 + 2));
+	if (new == NULL)
+		return (NULL);
+	for (i = 0; key[i] != '\0'; i++)
+		new[i] = key[i];
+	new[i] = '=';
+	for (j = 0; value[j] != '\0'; j++)
+		new[i + 1 + j] = value[j];
+	new[i + 1 + j] = '\0';
+	return (new);
 }
 
-
-
 /**
+ * find_key - To find an environment variable
+ * @env: Indicate array of environment variables
+ * @key: Indicate environment variable to find
  *
- *  * find_key - To find an environment variable
- *
- *   * @env: Indicate array of environment variables
- *
- *    * @key: Indicate environment variable to find
- *
- *     *
- *
- *      * Return: pointer to address of the environment variable
- *
- *       */
-
-
+ * Return: pointer to address of the environment variable
+ */
 
 char **find_key(char **env, char *key)
-
 {
+	unsigned int i, j, len;
 
-		unsigned int i, j, len;
-
-
-
-			len = _strlen(key);
-
-				for (i = 0; env[i] != NULL; i++)
-
-						{
-
-									for (j = 0; j < len; j++)
-
-												{
-
-																if (key[j] != env[i][j])
-
-																					break;
-
-																		}
-
-											if (j == len && env[i][j] == '=')
-
-															return (&env[i]);
-
-												}
-
-					return (NULL);
-
+	len = _strlen(key);
+	for (i = 0; env[i] != NULL; i++)
+	{
+		for (j = 0; j < len; j++)
+		{
+			if (key[j] != env[i][j])
+				break;
+		}
+		if (j == len && env[i][j] == '=')
+			return (&env[i]);
+	}
+	return (NULL);
 }
 
-
-
 /**
+ * add_key - To create a new environment variable
+ * @vars: Indicate pointer to struct of variables
  *
- *  * add_key - To create a new environment variable
- *
- *   * @vars: Indicate pointer to struct of variables
- *
- *    *
- *
- *     * Return: void
- *
- *      */
-
+ * Return: void
+ */
 void add_key(vars_t *vars)
-
 {
+	unsigned int i;
+	char **newenv;
 
-		unsigned int i;
-
-			char **newenv;
-
-
-
-				for (i = 0; vars->env[i] != NULL; i++)
-
-							;
-
-					newenv = malloc(sizeof(char *) * (i + 2));
-
-						if (newenv == NULL)
-
-								{
-
-											prints_error_msg(vars, NULL);
-
-													vars->status = 127;
-
-															new_exit(vars);
-
-																}
-
-							for (i = 0; vars->env[i] != NULL; i++)
-
-										newenv[i] = vars->env[i];
-
-								newenv[i] = add_value(vars->array_tokens[1], vars->array_tokens[2]);
-
-									if (newenv[i] == NULL)
-
-											{
-
-														prints_error_msg(vars, NULL);
-
-																free(vars->buffer);
-
-																		free(vars->commands);
-
-																				free(vars->array_tokens);
-
-																						free_env(vars->env);
-
-																								free(newenv);
-
-																										exit(127);
-
-																											}
-
-										newenv[i + 1] = NULL;
-
-											free(vars->env);
-
-												vars->env = newenv;
-
+	for (i = 0; vars->env[i] != NULL; i++)
+		;
+	newenv = malloc(sizeof(char *) * (i + 2));
+	if (newenv == NULL)
+	{
+		prints_error_msg(vars, NULL);
+		vars->status = 127;
+		new_exit(vars);
+	}
+	for (i = 0; vars->env[i] != NULL; i++)
+		newenv[i] = vars->env[i];
+	newenv[i] = add_value(vars->array_tokens[1], vars->array_tokens[2]);
+	if (newenv[i] == NULL)
+	{
+		prints_error_msg(vars, NULL);
+		free(vars->buffer);
+		free(vars->commands);
+		free(vars->array_tokens);
+		free_env(vars->env);
+		free(newenv);
+		exit(127);
+	}
+	newenv[i + 1] = NULL;
+	free(vars->env);
+	vars->env = newenv;
 }
