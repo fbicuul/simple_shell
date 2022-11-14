87b4f3a5b3302c41eb640a0d139d99c02e63429e
Felix <111078933+fbicuul@users.noreply.github.com>
2022-11-14 11:50:31 +0100

    Task on the simple shell

--- builtins.c
+++ builtins.c
@@ -1,1 +1,170 @@
-isd,dms,smd,sdssd
+#include "main.h"
+
+/**
+ * check_for_builtins - To check if the command is a builtin
+ * @vars: Indicate variables
+ * Return: pointer to the function or NULL
+ */
+void (*check_for_builtins(vars_t *vars))(vars_t *vars)
+{
+	unsigned int i;
+	builtins_t check[] = {
+		{"exit", new_exit},
+		{"env", _env},
+		{"setenv", new_setenv},
+		{"unsetenv", new_unsetenv},
+		{"cd", new_cd},
+		{"help", new_help},
+
+
+		{NULL, NULL}};
+
+	for (i = 0; check[i].f != NULL; i++)
+	{
+		/**vars->array_tokens is accessing the arguments for the match**/
+		if (_strcmpr(vars->array_tokens[0], check[i].name) == 0)
+			break;
+	}
+	if (check[i].f != NULL)
+		check[i].f(vars);
+	return (check[i].f);
+}
+
+/**
+ * new_exit - To exit program
+ * @vars: Indicate variables
+ * Return: void
+ */
+void new_exit(vars_t *vars)
+{
+
+	int status;
+	/*If exit has arguments, we handle it*/
+	if (_strcmpr(vars->array_tokens[0], "exit") ==
+			0 &&
+		vars->array_tokens[1] != NULL)
+
+	{
+		/* With this function we make sure that the entered number is valid */
+		status = _atoi(vars->array_tokens[1]);
+		/* we handle case of error*/
+		if (status == -1)
+		{
+			vars->status = 2;
+			/* will print an error message */
+			prints_error_msg(vars, ": Illegal number: ");
+			print_message(vars->array_tokens[1]);
+			print_message("\n");
+			return;
+		}
+		vars->status = status;
+	}
+	free(vars->commands);
+	free(vars->array_tokens);
+
+
+	free_env(vars->env);
+	free(vars->buffer);
+	exit(vars->status);
+}
+
+/**
+ * _env - To print the current environment
+ * @vars: Indicate struct of variables
+ * Return: void.
+ */
+void _env(vars_t *vars)
+{
+	unsigned int i;
+
+	for (i = 0; vars->env[i]; i++)
+	{
+		_puts(vars->env[i]);
+		_puts("\n");
+	}
+	vars->status = 0;
+}
+
+/*
+ * new_setenv - To create a new environment variable, or edit an existing variable
+ * @vars: Indicate pointer to struct of variables
+ *
+ * Return: void
+ */
+void new_setenv(vars_t *vars)
+{
+	char **key;
+	char *var;
+	/** if the arguments to setenv are wrong we print the error message **/
+	if (vars->array_tokens[1] == NULL || vars->array_tokens[2] == NULL)
+	{
+
+		prints_error_msg(vars, ": Incorrect number of arguments\n");
+		vars->status = 2;
+		return;
+	}
+	key = find_key(vars->env, vars->array_tokens[1]);
+	if (key == NULL)
+	{
+		add_key(vars);
+	}
+	else
+	{
+		var = add_value(vars->array_tokens[1], vars->array_tokens[2]);
+		if (var == NULL)
+		{
+			prints_error_msg(vars, NULL);
+			free(vars->buffer);
+			free(vars->array_tokens);
+			free(vars->commands);
+			free_env(vars->env);
+			exit(127);
+		}
+		free(*key);
+		*key = var;
+	}
+	vars->status = 0;
+}
+
+/**
+ * new_unsetenv - To remove an environment variable
+ * @vars: Indicate pointer to a struct of variables
+ *
+ * Return: void
+ */
+void new_unsetenv(vars_t *vars)
+{
+	char **key, **newenv;
+	unsigned int i, j;
+
+	if (vars->array_tokens[1] == NULL)
+	{
+		prints_error_msg(vars, ": Incorrect number of arguments\n");
+		vars->status = 2;
+		return;
+	}
+	key = find_key(vars->env, vars->array_tokens[1]);
+	if (key == NULL)
+	{
+		prints_error_msg(vars, ": No variable to unset");
+		return;
+	}
+	for (i = 0; vars->env[i] != NULL; i++)
+		;
+	newenv = malloc(sizeof(char *) * i);
+	if (newenv == NULL)
+	{
+		prints_error_msg(vars, NULL);
+		vars->status = 127;
+		new_exit(vars);
+	}
+	for (i = 0; vars->env[i] != *key; i++)
+		newenv[i] = vars->env[i];
+	for (j = i + 1; vars->env[j] != NULL; j++, i++)
+		newenv[i] = vars->env[j];
+	newenv[i] = NULL;
+	free(*key);
+	free(vars->env);
+	vars->env = newenv;
+	vars->status = 0;
+}
