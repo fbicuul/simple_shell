82cb15e5673677ae9965ef6aad04e9f323b31699
Felix <111078933+fbicuul@users.noreply.github.com>
2022-11-14 11:47:01 +0100

    Task on the simple shell

--- cd_add_functions.c
+++ cd_add_functions.c
@@ -1,1 +1,28 @@
-iinbvdnbvandadlad
+#include "main.h"
+
+/**
+ * print_cd_1 - To print error when cd argument in invalid
+ *              i.e. cd -asd22
+ * @vars: Indicate vars parameter to include
+ * Return: Always return void and print a messaje as shown below.
+ */
+void print_cd_1(vars_t *vars)
+{
+	print_str("-bash: cd: ", 1);
+	print_str(vars->array_tokens[1], 1);
+	print_str(": invalid option", 0);
+	print_str("cd: usage: cd [-] [--]", 0);
+}
+
+/**
+ * print_cd_2 - To print error message when cd argument doesn'n exist.
+ *              i.e. cd das7d32
+ * @vars: Indicate vars parameter to include
+ * Return: Always return void and print a messaje as shown below.
+ */
+void print_cd_2(vars_t *vars)
+{
+	print_str("-bash: cd: ", 1);
+	print_str(vars->array_tokens[1], 1);
+	print_str(": Not a directory", 0);
+}
